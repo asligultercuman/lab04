5a0e9c38aa665052d6e53101455868d811a2e1f8
Asligul Tercuman <asligultercumann@gmail.com>
2024-11-20 12:17:51 +0300

    constructor, AddFront, RemoveFront fonksiyonları yapıldı

--- Deque.cpp
+++ Deque.cpp
@@ -3,8 +3,10 @@
 ///------------------------------------------------------
 /// Default constructor
 /// 
-Deque::Deque() {
-	// Fill this in
+Deque::Deque() { //Asligul
+	head = NULL;
+	tail = NULL;
+	noOfItems = 0;
 } //end-Deque
 
 ///------------------------------------------------------
@@ -17,8 +19,18 @@ Deque::~Deque() {
 ///------------------------------------------------------
 /// Adds a new item to the front of the Deque
 /// 
-void Deque::AddFront(int item) {
-	// Fill this in
+void Deque::AddFront(int item) { //Asligul
+	DequeNode* n = new DequeNode(item);
+	if (head == NULL)//if doubly linked list is empty
+	{
+		head = n;
+		tail = n;
+	}
+	else {
+		n->next = head;
+		head->prev = n;
+		head = n;
+	}
 } //end-AddFront
 
 ///------------------------------------------------------
@@ -32,9 +44,22 @@ void Deque::AddRear(int item) {
 /// Remove and return the item at the front of the Deque
 /// If the Deque is empty, throw an exception
 /// 
-int Deque::RemoveFront() {
-	// Fill this in
-	return 0;
+int Deque::RemoveFront() { //Asligul
+	if (head == NULL)
+	throw(0);
+
+	int tempData = head->item;
+	DequeNode* temp = head; 
+
+	head = head->next;
+
+	if (head != NULL)
+		head->prev = NULL;
+	else
+		tail = NULL; //edge case : the removed part could be the last node of the list
+
+	delete temp;
+	return tempData;
 } //end-RemoveFront
 
 ///------------------------------------------------------
