ed94696cfaef9b89e99f0fa9227553ad7a059843
Asligul Tercuman <asligultercumann@gmail.com>
2024-11-23 09:19:04 +0300

    addFront ve addRear fonksiyonlarında düzeltmeler yapıldı

--- Deque.cpp
+++ Deque.cpp
@@ -1,4 +1,5 @@
 #include "Deque.h"
+#include <stdexcept>
 
 ///------------------------------------------------------
 /// Default constructor
@@ -12,9 +13,22 @@ Deque::Deque() { //Asligul
 ///------------------------------------------------------
 /// Destructor
 /// 
-Deque::~Deque() {
-	// Fill this in
-} //end-~Deque
+Deque::~Deque() { // Elif
+	
+	while (head != NULL) {
+		DequeNode* temp = head;
+		head = head->next;
+		delete temp;
+	}
+	tail = NULL;
+	noOfItems = 0;
+
+	/*
+	head = nullptr;  // Just reset the pointers
+	tail = nullptr;
+	noOfItems = 0;
+	*/
+}
 
 ///------------------------------------------------------
 /// Adds a new item to the front of the Deque
@@ -23,21 +37,31 @@ void Deque::AddFront(int item) { //Asligul
 	DequeNode* n = new DequeNode(item);
 	if (head == NULL)//if doubly linked list is empty
 	{
-		head = n;
-		tail = n;
+		head = tail = n;
 	}
 	else {
 		n->next = head;
 		head->prev = n;
 		head = n;
 	}
+	noOfItems++;//eleman sayisi guncellenir
 } //end-AddFront
 
 ///------------------------------------------------------
 /// Adds a new item to the end of the Deque
 /// 
-void Deque::AddRear(int item) {
-	// Fill this in
+void Deque::AddRear(int item) {     //Özge
+	DequeNode* n = new DequeNode(item);
+
+	if (head == nullptr) {
+		head = tail = n;
+	}
+	else {
+		tail->next = n;
+		n->prev = tail;
+		tail = n;
+	}
+	noOfItems++;//eleman sayisi guncellenir
 } //end-AddRear
 
 ///------------------------------------------------------
@@ -46,19 +70,19 @@ void Deque::AddRear(int item) {
 /// 
 int Deque::RemoveFront() { //Asligul
 	if (head == NULL)
-	throw(0);
+		throw std::runtime_error("Deque is empty!");
 
 	int tempData = head->item;
-	DequeNode* temp = head; 
-
+	DequeNode* temp = head;
 	head = head->next;
 
 	if (head != NULL)
 		head->prev = NULL;
 	else
-		tail = NULL; //edge case : the removed part could be the last node of the list
+		tail = NULL; //edge case : the removed part could be the last node
 
 	delete temp;
+	noOfItems--;//eleman sayisi guncellenir
 	return tempData;
 } //end-RemoveFront
 
@@ -66,17 +90,35 @@ int Deque::RemoveFront() { //Asligul
 /// Remove and return the item at the rear of the Deque
 /// If the Deque is empty, throw an exception
 /// 
-int Deque::RemoveRear() {
-	// Fill this in
-	return 0;
+int Deque::RemoveRear() {//Elif
+	if (tail == NULL)
+		throw std::runtime_error("Deque is empty!");
+	int tempData = tail->item;
+	DequeNode* temp = tail;
+	tail = tail->prev;
+
+	if (tail != NULL)
+		tail->next = NULL;
+	else
+		head = NULL;
+
+	delete temp;
+	noOfItems--;
+	return tempData;
+
 } //end-RemoveRear
 
 ///------------------------------------------------------
 /// Return the item at the front of the Deque (do not remove the item)
 /// If the Deque is empty, throw an exception
 /// 
-int Deque::Front() {
-	// Fill this in
+int Deque::Front() {    //Özge
+	if (head == nullptr) {
+		throw std::runtime_error("Deque is empty!");
+	}
+	else {
+		return head->item;
+	}
 	return 0;
 } //end-Front
 
@@ -84,7 +126,12 @@ int Deque::Front() {
 /// Return the item at the rear of the Deque (do not remove the item)
 /// If the Deque is empty, throw an exception
 /// 
-int Deque::Rear() {
-	// Fill this in
+int Deque::Rear() {     //Özge
+	if (tail == nullptr) {
+		throw std::runtime_error("Deque is empty!");
+	}
+	else {
+		return tail->item;
+	}
 	return 0;
 } //end-Rear
